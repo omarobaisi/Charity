e6f40351d010624aaffea41a835fd627cb935d0c
omarobaisi <omr.obasi28@gmail.com>
2022-08-09 13:04:27 +0300

    cleanCode

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,43 +1,42 @@
 const express = require("express");
 const router = express.Router();
-//const APIKey
 const Charity = require("../models/charity");
 const axios = require("axios");
 
 const getCharityInfo = async () => {
   try {
-    let charityInfo = await axios.get(
+    return await axios.get(
       `https://api.data.charitynavigator.org/v2/Organizations?app_id=d54cad3f&app_key=202cc4e34ec2318a42feb48a2ffe8424`
     );
-    console.log(charityInfo);
   } catch (e) {
     console.log(e);
   }
 };
-//getCharityInfo();
+
+const orgenizeAPI = (data) => {
+  const charities = []
+  for(let i=50; i<100; i++) {
+    if(data[i].irsClassification) {
+      let newCharity = {
+        name: data[i].charityName,
+        classification: data[i].irsClassification.classification,
+        description: data[i].irsClassification.affiliation,
+        website: data[i].charityNavigatorURL,
+      };
+      charities.push(newCharity)
+    }
+  }
+  return charities
+}
+
 router.get("/charities", async function (request, response) {
   try {
     let charityInfo = await getCharityInfo();
-    console.log(charityInfo)
-    response.end();
+    const charities = orgenizeAPI(charityInfo.data)
+    response.send(charities);
   } catch(e) {
       console.log(e)
   }
-  
-  // charityInfo
-  //   .then(function (resp) {
-  //     //let charityInfo = resp.data;
-  //     let charity = {
-  //       name: charityInfo.charityName,
-  //       classification: charityInfo.irsClassification.classification,
-  //       description: charityInfo.irsClassification.affiliation,
-  //       website: charityInfo.websiteURL,
-  //     };
-  //     response.send(charity);
-  //   })
-  //   .catch(function (error) {
-  //     response.status(404).send({ error: "The city name does not exist" });
-  //   });
 });
 
 module.exports = router;
