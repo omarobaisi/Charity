82dad11cb216cc2df4a4928d11a54f029a92cf53
Zahra'a Aqel <zmoudmah@gmail.com>
2022-08-09 15:12:06 +0300

    need to pull

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-//const APIKey
 const Charity = require("../models/charity");
 const axios = require("axios");
 
@@ -9,35 +8,42 @@ const getCharityInfo = async () => {
     let charityInfo = await axios.get(
       `https://api.data.charitynavigator.org/v2/Organizations?app_id=d54cad3f&app_key=202cc4e34ec2318a42feb48a2ffe8424`
     );
-    //console.log(charityInfo);
     return charityInfo;
   } catch (e) {
     console.log(e);
   }
 };
-//getCharityInfo();
+
 router.get("/charities", async function (request, response) {
   try {
     let charityInfo = await getCharityInfo();
-    const charities = []
-    charityInfo = charityInfo.data
-    // console.log(charityInfo.length)
-    // response.end();
-    for(let i=50; i<100; i++) {
-      if(charityInfo[i].irsClassification) {
+    const charities = [];
+    charityInfo = charityInfo.data;
+    for (let i = 50; i < 100; i++) {
+      if (charityInfo[i].irsClassification) {
         let newCharity = {
           name: charityInfo[i].charityName,
           classification: charityInfo[i].irsClassification.classification,
           description: charityInfo[i].irsClassification.affiliation,
-          website: charityInfo[i].websiteURL,
+          // website: charityInfo[i].websiteURL,
         };
-        charities.push(newCharity)
+        charities.push(newCharity);
       }
     }
     response.send(charities);
-  } catch(e) {
-      console.log(e)
+  } catch (e) {
+    console.log(e);
   }
+});
+
+router.get("/charities/:classification", async function (request, response) {
+  let classification = request.params.classification;
+ const charities = await Charity.find(
+    {
+      name: classification,
+    }
+  );
+  response.send(charities)
 });
 
 module.exports = router;
