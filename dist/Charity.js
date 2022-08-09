2c71366d9fdf13782516847b762d40e31841bb3d
omarobaisi <omr.obasi28@gmail.com>
2022-08-09 16:09:32 +0300

    organizing

--- dist/Charity.js
+++ dist/Charity.js
@@ -6,34 +6,4 @@ const Charity = class {
     console.log(data);
     renderer.render(data);
   }
-  getClassificationCharity() {
-    let charityClassification = $("#charityClassification").val();
-    $.ajax({
-      type: "get",
-      cache: false,
-      url: `/getCharities/${charityClassification}`,
-      error: function (request, error) {
-        alert(" charity Not found or invlid input");
-        return;
-      },
-      success: function (response) {
-        render.render(response);
-      },
-    });
-  }
-  getSpecificCharity() {
-    let charitySpecific = $("#charitySpecific").val();
-    $.ajax({
-      type: "get",
-      cache: false,
-      url: `/getCharity/${charitySpecific}`,
-      error: function (request, error) {
-        alert(" charity Not found or invlid input");
-        return;
-      },
-      success: function (response) {
-        render.render(response);
-      },
-    });
-  }
 };
