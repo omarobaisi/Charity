b09d677f9d7f6d7090ffdce610cfa9fe44b04cb3
omarobaisi <omr.obasi28@gmail.com>
2022-08-11 06:48:51 +0300

    finishing the styling

--- dist/CharityApi.js
+++ dist/CharityApi.js
@@ -5,7 +5,7 @@ const CharityApi = class {
   async getAllCharity() {
     this.data = await $.get("/getCharities");
     this.data.forEach(charity => {
-      charity.img = this.getRandomImage();
+      charity.img = getRandomImage();
     })
     console.log(this.data);
     const renderer = new Renderer(
@@ -37,6 +37,11 @@ const CharityApi = class {
         return;
       },
       success: function (response) {
+        console.log(response)
+        response.forEach(charity => {
+          charity.img = getRandomImage();
+          console.log(charity)
+        })
         const renderer = new Renderer(
           "#charities-container",
           "#charities-template"
@@ -82,17 +87,5 @@ const CharityApi = class {
       $("#charities-template")
     );
     renderer.renderCharities(newArray);
-  }
-  getRandomImage() {
-    let images = ["https://img.freepik.com/free-vector/flat-ramadan-charity-background-with-muslim-people-giving-money-food-hungry-homeless-illustration_1284-61988.jpg?w=996&t=st=1660153693~exp=1660154293~hmac=b2ffe555ef58541812313c31897120f5119da34363fa607a4161701b22cb5fcc", 
-    "https://static.vecteezy.com/system/resources/previews/001/880/049/large_2x/volunteers-collecting-donations-for-charity-free-vector.jpg", 
-    "https://img.freepik.com/premium-vector/hand-puts-money-coin-into-charity-box-concept-charity-caring-people-illustration-flat-style_198838-259.jpg?w=740", 
-    "https://img.freepik.com/premium-vector/homeless-man-with-his-dog-needing-job-cartoon-vector-illustration-isolated-white-background_223337-3349.jpg?w=740", 
-    "https://i.pinimg.com/564x/c6/39/4b/c6394b2d857fc6435845139116a5de82.jpg",
-    "https://i.pinimg.com/564x/c6/39/4b/c6394b2d857fc6435845139116a5de82.jpg",
-    "https://img.freepik.com/free-vector/drawn-clothing-donation-concept_23-2148832529.jpg?w=740&t=st=1660154503~exp=1660155103~hmac=996920e85153a7391671073db78d384789d2e7c9b8df3a89a6e4eb9c5da72426"]
-
-    var image = images[Math.floor(Math.random()*images.length)];
-    return image
   }
 };
