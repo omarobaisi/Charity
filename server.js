94d50506dc5a499e45f2badc82512fa19749975a
omarobaisi <omr.obasi28@gmail.com>
2022-08-19 23:46:26 +0300

    deploying production

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require("express");
 const path = require("path");
 const api = require("./server/routes/api");
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.static(path.join(__dirname, "node_modules")));
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/charity", { useNewUrlParser: true });
+const url = process.env.MONGODB_URI || "mongodb://localhost/yelp_camp";
+mongoose.connect(url)
+
 app.use("/", api);
-const port = 3000;
-app.listen(port, function () {
-  console.log(`Server running on ${port}`);
+
+const PORT = 3000;
+app.listen(process.env.PORT || PORT, function () {
+  console.log(`Server running on ${PORT}`);
 });
